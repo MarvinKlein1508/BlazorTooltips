57a9bb57f6d86c8c9e9ffa1a896aff906113bfa4
MarvinKlein1508 <support@marvinkleinmusic.de>
2023-09-04 13:50:57 +0200

    Update demos

--- BlazorTooltips/Tooltip.razor.cs
+++ BlazorTooltips/Tooltip.razor.cs
@@ -1,47 +1,93 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Dynamic;
+using System.Text.Json;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 
 namespace BlazorTooltips
 {
     public partial class Tooltip : IAsyncDisposable
     {
-        [Parameter] public TooltipOptions Options { get; set; } = new TooltipOptions();
-        [Parameter] public RenderFragment? ChildContent { get; set; }
-
-        [Parameter] public string Title { get; set; } = String.Empty;
-        [Parameter] public bool Html { get; set; } = true;
-        [Parameter] public TooltipPlacement Placement { get; set; } = TooltipPlacement.Top;
-
-        [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object> AllOtherAttributes { get; set; } = new Dictionary<string, object>();
         private Guid _id;
-
         private bool _rendered = false;
-        private IJSObjectReference? jsModule { get; set; }
+        private IJSObjectReference? _jsModule { get; set; }
+        #region Parameters
 
+        /// <summary>
+        /// Sets the text of the tooltip.
+        /// </summary>
+        [Parameter, EditorRequired]
+        public string Title { get; set; } = String.Empty;
+        /// <summary>
+        /// Enables HTML Content in the tooltip. 
+        /// <para>
+        /// Default is <c>true</c>.
+        /// </para>
+        /// </summary>
+        [Parameter] public bool Html { get; set; } = true;
+        /// <summary>
+        /// Sets the content which the tooltip should wrap around.
+        /// </summary>
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+        /// <summary>
+        /// Sets the default positioning of the tooltip.
+        /// <para>
+        /// Default is <see cref="TooltipPlacement.Top"/>
+        /// </para>
+        /// </summary>
+        [Parameter] public TooltipPlacement Placement { get; set; } = TooltipPlacement.Top;
+        /// <summary>
+        /// Sets the bootstrap custom class property
+        /// 
+        /// <para>
+        /// Default is <c>string.Empty</c>
+        /// </para>
+        /// 
+        /// <para>
+        /// Requires at least Bootstrap 5.2 or later.
+        /// </para>
+        /// </summary>
+        [Parameter] public string CustomClass { get; set; } = string.Empty;
+        /// <summary>
+        /// Apply a CSS fade transition to the tooltip
+        /// <para>
+        /// Default is <c>true</c>
+        /// </para>
+        /// </summary>
+        [Parameter] public bool Animation { get; set; } = true;
+        /// <summary>
+        /// Delay showing and hiding the tooltip (ms)—doesn’t apply to manual trigger type. If a number is supplied, delay is applied to both hide/show.   
+        /// </summary>
+        [Parameter] public int Delay { get; set; } = 0;
+        [Parameter] public string Container { get; set; } = string.Empty;
+        [Parameter] public string Selector { get; set; } = string.Empty;
+        [Parameter] public string Template { get; set; } = "<div class='tooltip' role='tooltip'><div class='tooltip-arrow'></div><div class='tooltip-inner'></div></div>";
+        [Parameter] public TooltipTrigger[] Trigger { get; set; } = new TooltipTrigger[] { TooltipTrigger.Hover };
+        [Parameter] public TooltipPlacement[] FallbackPlacements { get; set; } = new TooltipPlacement[] { TooltipPlacement.Top, TooltipPlacement.Right, TooltipPlacement.Bottom, TooltipPlacement.Left };
+        [Parameter] public string Boundary { get; set; } = "clippingParents";
+        [Parameter] public bool Sanitize { get; set; } = true;
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AllOtherAttributes { get; set; } = new Dictionary<string, object>();
+        #endregion
         public Tooltip()
         {
             _id = Guid.NewGuid();
         }
-
-
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
                 _rendered = true;
-                jsModule = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BlazorTooltips/bootstrap.tooltip.js");
-                await Setup(Options);
+                _jsModule = await jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BlazorTooltips/bootstrap.tooltip.js");
+                await Setup();
             }
         }
         protected override async Task OnParametersSetAsync()
         {
             if (_rendered)
+            {
                 await Update();
+            }
         }
 
         public async ValueTask DisposeAsync()
@@ -49,45 +95,65 @@ namespace BlazorTooltips
             await Destroy();
         }
 
-        private async Task Setup(TooltipOptions options)
+        private async Task Setup()
         {
-            if (jsModule is not null)
+            if (_jsModule is not null)
             {
-                await jsModule.InvokeVoidAsync("setup", new object[] { _id, GetOptions().ToJSON() });
+                await _jsModule.InvokeVoidAsync("setup", new object[] { _id, OptionsToJSON() });
             }
         }
 
         private async Task Destroy()
         {
-            if (jsModule is not null)
+            if (_jsModule is not null)
             {
-                try
-                {
-                    await jsModule.InvokeVoidAsync("destroy", _id);
-                    await jsModule.DisposeAsync();
-                }
-                catch (TaskCanceledException)
-                {
-                }
-                catch (JSDisconnectedException)
-                {
-                }
+                await _jsModule.InvokeVoidAsync("destroy", _id);
+                await _jsModule.DisposeAsync();
             }
         }
 
-        private TooltipOptions GetOptions()
+        private string OptionsToJSON()
         {
-            Options.Html = Html;
-            Options.Title = Title;
-            Options.Placement = Placement;
-            return Options;
+            dynamic tmp = new ExpandoObject();
+            tmp.animation = Animation;
+            tmp.delay = Delay;
+            tmp.html = Html;
+            tmp.placement = Placement.ToString().ToLower();
+            tmp.template = Template;
+            tmp.title = Title;
+            tmp.trigger = String.Join(' ', Trigger.Distinct().Select(x => x.ToString().ToLower()));
+            tmp.fallbackPlacements = FallbackPlacements.Select(x => x.ToString().ToLower()).ToArray();
+            tmp.boundary = Boundary;
+            tmp.customClass = CustomClass;
+            tmp.sanitize = Sanitize;
+            tmp.closest = false;
+
+            if (String.IsNullOrWhiteSpace(Container))
+            {
+                tmp.container = false;
+            }
+            else
+            {
+                tmp.container = Container;
+            }
+
+            if (String.IsNullOrWhiteSpace(Selector))
+            {
+                tmp.selector = false;
+            }
+            else
+            {
+                tmp.selector = Selector;
+            }
+
+            return JsonSerializer.Serialize(tmp);
         }
 
         private async Task Update()
         {
-            if (jsModule is not null)
+            if (_jsModule is not null)
             {
-                await jsModule.InvokeVoidAsync("update", new object[] { _id, GetOptions().ToJSON() });
+                await _jsModule.InvokeVoidAsync("update", new object[] { _id, OptionsToJSON() });
             }
         }
 
